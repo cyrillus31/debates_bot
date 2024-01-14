ff5b0c4901e0dd229218a1d0d91460d2e3a8245f
cyrillus31 <kirill.olegovich31@gmail.com>
2024-01-14 14:56:19 +0000

    mvp production ready version

--- src/handlers/callback_handlers.py
+++ src/handlers/callback_handlers.py
@@ -1,4 +1,4 @@
-from telebot import types
+from telebot import types, apihelper
 
 from bot import bot
 from polls import Poll
@@ -15,15 +15,20 @@ def edit_poll(call: types.CallbackQuery):
     message = call.message
         
     edited_poll = Poll(prev_message=message.text, 
-                       username=user.first_name, 
+                       username=user.username, 
+                       user_first_name=user.first_name,
                        user_side=call.data,
                        )
 
-    _edit_poll(text=edited_poll.generate_body(), 
-               message_id=message.id, 
-               chat_id=message.chat.id,
-               reply_markup=edited_poll.generate_markup()
-               )
+
+    try:
+        _edit_poll(text=edited_poll.generate_body(), 
+                   message_id=message.id, 
+                   chat_id=message.chat.id,
+                   reply_markup=edited_poll.generate_markup()
+                   )
+    except apihelper.ApiTelegramException as e:
+        print(f"The following message was not updated:\n\"{message.text}\"")
 
 
     
--- src/polls.py
+++ src/polls.py
@@ -8,11 +8,18 @@ class Poll:
     AGAINST = "Against: %s\n"
     NOBODY = "NOBODY YET"
 
-    def __init__(self, prev_message: str = "", topic: str = "", username: str = "", user_side: str = "", language: str = "RU"):
+    def __init__(self, prev_message: str = "", 
+                 topic: str = "", 
+                 username: str = "", 
+                 user_first_name: str = "", 
+                 user_side: str = "", 
+                 language: str = "RU",
+                ):
+
         self.language = language
         self.prev_message = prev_message 
         self.topic = topic 
-        self.username = username
+        self.username = f"{user_first_name} ({username})"
         self.user_side = user_side
         self.proponent = "NOBODY YET"
         self.opponent = "NOBODY YET"
@@ -21,13 +28,19 @@ class Poll:
     @staticmethod
     def _parse_prev_message(prev_message: str) -> dict:
         # exp = r"\s*(.+)\s+(?:For: )(.+)\s+(?:Against: )(.+)"
-        exp = r"\s*(.+)\s*(?:For: )(.+)"
+        exp = r"\s*(.+)\s*(?:[For: ]+)(.+)\s+(?:[Against: ]+)(.+)"
         r = re.compile(exp)
-        print(repr(prev_message), "!!!!!!!!!!!!!!!!!!!!!!!")
-        print(r.search(repr(prev_message)))
-        topic, proponent, opponent = r.search(prev_message).groups()
+        m = r.search(prev_message)
+        topic, proponent, opponent = m.groups()
+        prev_msg_dict = {
+                "topic": topic.strip(), 
+                "proponent": proponent.strip(), 
+                "opponent": opponent.strip()
+                }
 
-        return {"topic": topic, "proponent": proponent, "opponent": opponent}
+        print(prev_msg_dict)
+
+        return prev_msg_dict
 
     def generate_body(self) -> str:
         if self.prev_message:
@@ -36,14 +49,20 @@ class Poll:
                 self.topic = prev_msg_dict["topic"]
                 self.proponent = prev_msg_dict["proponent"]
                 self.opponent = prev_msg_dict["opponent"]
-            except ValueError as e:
+            except (ValueError, AttributeError) as e:
                 self.error_message = f"!! Error: {e} !!"
+                
 
         if self.user_side and self.username:
             if self.user_side == "for":
                 self.proponent = self.username
+                if self.opponent == self.username:
+                    self.opponent = Poll.NOBODY
+
             elif self.user_side == "against":
                 self.opponent = self.username
+                if self.proponent == self.username:
+                    self.proponent = Poll.NOBODY
 
         body = f"{self.topic}\n{self.error_message}\nFor : {self.proponent}\nAgainst : {self.opponent}"
         return body
@@ -53,7 +72,7 @@ class Poll:
         buttons = [
             types.InlineKeyboardButton("For", callback_data="for"),
             types.InlineKeyboardButton("Against", callback_data="against"),
-            types.InlineKeyboardButton("Next", callback_data="next"),
+            # types.InlineKeyboardButton("Next", callback_data="next"),
             ]
         markup.add(*buttons)
         return markup
